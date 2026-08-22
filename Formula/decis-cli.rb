a62b4ced854f955bc377f4352234dd014ff3cfc5
Taeho Kim <taeho@taeho.io>
2026-08-22 09:52:02 +0000

    chore(decis-cli): update to 1.2.0

--- Formula/decis-cli.rb
+++ Formula/decis-cli.rb
@@ -9,11 +9,11 @@ class DecisCli < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tab0inc/decis-cli-releases/releases/download/v1.1.0/decis-cli_1.1.0_darwin_arm64.tar.gz"
-      sha256 "d7858aaea651c821edc42a2d479ea1d28600c559d840b59134125fd498547d94"
+      url "https://github.com/tab0inc/decis-cli-releases/releases/download/v1.2.0/decis-cli_1.2.0_darwin_arm64.tar.gz"
+      sha256 "f062e2bb1eee1e2a8d262f5c8daa94335983875e8c764da55d2b4fc6aa1cfc7b"
     else
-      url "https://github.com/tab0inc/decis-cli-releases/releases/download/v1.1.0/decis-cli_1.1.0_darwin_amd64.tar.gz"
-      sha256 "890cbf3301076cc9c11cc19bde9fdbbe930e959577658a9a68fceac663850577"
+      url "https://github.com/tab0inc/decis-cli-releases/releases/download/v1.2.0/decis-cli_1.2.0_darwin_amd64.tar.gz"
+      sha256 "a6f90c5eb9bfdac34bf48c410096a4630ae46f50ce5a6f4f9dbd30d8dce79991"
     end
   end
 
